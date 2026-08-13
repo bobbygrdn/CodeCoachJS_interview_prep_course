3ac8d89080cb730a86df3a95118a7c365c95f742
bobbygrdn <bobbygrdn777@gmail.com>
2026-08-13 15:09:49 -0700

    finished map_filter_reduce

--- src/map_filter_reduce/my_array_methods.js
+++ src/map_filter_reduce/my_array_methods.js
@@ -3,23 +3,47 @@
  * PLEASE DO NOT USE THE BUILT IN METHODS FOR THE FOLLOWING EXERCISES
  */
 Array.prototype.myFilter = function (callBack) {
-  //add your code here
+  newArray = [];
+  for(i = 0; i < this.length; i++) {
+    if(callBack(this[i])){
+      newArray.push(this[i]);
+    }
+  }
+  return newArray;
 };
 
 Array.prototype.myMap = function (callBack) {
-  //add your code here
+  newArray = [];
+  for(i = 0;i < this.length; i++) {
+    newArray.push(callBack(this[i]));
+  }
+  return newArray;
 };
 
 Array.prototype.myEvery = function (callBack) {
-  //add your code here
+  flag = true;
+  for(i = 0; i < this.length; i++) {
+    if(!callBack(this[i])) {
+      flag = false;
+    }
+  }
+  return flag;
 };
 
 Array.prototype.mySome = function (callBack) {
-  //add your code here
+  flag = false;
+  for(i = 0; i < this.length; i++) {
+    if(callBack(this[i])) {
+      flag = true;
+      break;
+    }
+  }
+  return flag;
 };
 
 const addAllNumbers = (arr) => {
   //add your code here using reduce
+  return arr.reduce((prev, next) => prev + next);
 };
 
 module.exports = {
